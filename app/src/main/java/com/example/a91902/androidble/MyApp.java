be58b9ace7cb2fc56260415884e0da56d6d2ceb6
Hetansh-1997 <hetansh1997@gmail.com>
2019-02-01 09:58:02 +0530

    Ui Changes

--- app/src/main/java/com/example/a91902/androidble/MyApp.java
+++ app/src/main/java/com/example/a91902/androidble/MyApp.java
@@ -3,16 +3,34 @@ package com.example.a91902.androidble;
 import android.app.Application;
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MyApp extends Application {
-    private static Context context;
+    //private static Context context;
+    private RequestQueue requestQueue;
+    private static MyApp mInstance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        context=getApplicationContext();
+        mInstance=this;
     }
 
-    public static Context getContext(){
-        return context;
+    public static synchronized MyApp getInstance() {
+        return mInstance;
+    }
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null)
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        return requestQueue;
+    }
+    public void addToRequestQueue(Request request, String tag) {
+        request.setTag(tag);
+        getRequestQueue().add(request);
+    }
+    public void cancelAllRequests(String tag) {
+        getRequestQueue().cancelAll(tag);
     }
 }
--- app/src/main/java/com/example/a91902/androidble/login/SignInActivity.java
+++ app/src/main/java/com/example/a91902/androidble/login/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.example.a91902.androidble.login;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +9,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.a91902.androidble.bluetooth.BeaconConnection;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.a91902.androidble.MyApp;
 import com.example.a91902.androidble.R;
-public class SignInActivity extends AppCompatActivity {
+import com.example.a91902.androidble.home.ProductActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SignInActivity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
     TextView textViewPhone,textViewPassword;
     AlertDialog.Builder builder;
     @Override
@@ -20,6 +30,8 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         textViewPhone= findViewById(R.id.edit_text_phone_number);
         textViewPassword= findViewById(R.id.edit_text_password);
+        final String[] json = new String[1];
+        final String url = "https://ecom-kalpesh.herokuapp.com/notes";
         findViewById(R.id.text_view_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,14 +42,42 @@ public class SignInActivity extends AppCompatActivity {
                 }else if(password.isEmpty()){
                     dialogbox("Enter Phone Password");
                 }else{
-                    startActivity(new Intent(SignInActivity.this,BeaconConnection.class));
+                    progressDialog=new ProgressDialog(SignInActivity.this);
+                    progressDialog.setMessage("Validating");
+                    progressDialog.show();
+                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                            url, null,
+                            new Response.Listener() {
+                                @Override
+                                public void onResponse(Object response) {
+                                    json[0] =response.toString();
+                                        try {
+                                            JSONObject jsonObject=new JSONObject(json[0]);
+                                            String str =jsonObject.getString("Title");
+                                            if(str.equals("this is an Awesome WebPage")){
+                                                startActivity(new Intent(SignInActivity.this,ProductActivity.class));
+                                                progressDialog.dismiss();
+                                                finish();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    dialogbox("Please Turn on the internet");
+                                }
+                            });
+                    MyApp.getInstance().addToRequestQueue(jsonObjReq, "getRequest");
                 }
             }
         });
     }
 
     public void dialogbox(String text){
-        builder=new AlertDialog.Builder(this);
+        builder=new AlertDialog.Builder(SignInActivity.this);
         builder.setMessage(text)
                 .setCancelable(false)
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
